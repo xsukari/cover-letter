b9d9101adcf0908614d25f29ccf49f4e38c39467
xsukari <43860627+xsukari@users.noreply.github.com>
2024-05-13 18:30:48 +0200

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import * as data from "./components/data"
+
+const _recipient = data.getData(data.categories.recipient) as data.Recipient
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: "CV",
-    description: "résumé",
+    title: "cover letter - " + _recipient.company,
+    description: "cover letter",
 }
 
 export default function RootLayout({
